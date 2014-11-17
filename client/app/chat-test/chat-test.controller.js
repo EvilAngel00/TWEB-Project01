5d622144a0a37caf240c12d968bb85487bd83f10
Rui Reis <dante2308@gmail.com>
2014-11-17 16:35:57 +0100

    Message filtering working

--- client/app/chat-test/chat-test.controller.js
+++ client/app/chat-test/chat-test.controller.js
@@ -1,32 +1,37 @@
 'use strict';
 
 angular.module('twebProject01App')
-  .controller('ChatTestCtrl', function ($scope, $http, socket, Auth) {
-    $scope.allMessages = [];
-	
-	$http.get('/api/messages').success(function(allMessages) {
-      $scope.allMessages = allMessages;
-      socket.syncUpdates('message', $scope.allMessages);
+    .controller('ChatTestCtrl', function ($scope, $http, socket, Auth, $location) {
+        $scope.allMessages = [];
+        $scope.classroomId = $location.search().id;
+
+        $http.get('/api/messages').success(function (allMessages) {
+            $scope.allMessages = allMessages;
+            socket.syncUpdates('message', $scope.allMessages);
+        });
+
+        $scope.addMessage = function () {
+            if (this.newMessage === '') {
+                return;
+            }
+            $http.post('/api/messages', {
+                content: this.newMessage,
+                user: Auth.getCurrentUser().name,
+                classroomId: $scope.classroomId
+            });
+            this.newMessage = '';
+        };
+
+        $scope.deleteMessage = function (message) {
+            $http.delete('/api/messages/' + message._id);
+        };
+
+        $scope.$on('$destroy', function () {
+            socket.unsyncUpdates('message');
+        });
+
+        $scope.scroll = function () {
+            var box = document.getElementById('chatDiv');
+            box.scrollTop = box.scrollHeight;
+        };
     });
-
-    $scope.addMessage = function() {
-      if(this.newMessage === '') {
-        return;
-      }
-      $http.post('/api/messages', { content: this.newMessage, user: Auth.getCurrentUser().name });
-      this.newMessage = '';
-    };
-
-    $scope.deleteMessage = function(message) {
-      $http.delete('/api/messages/' + message._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('message');
-    });
-	
-	$scope.scroll = function() {
-		var box = document.getElementById('chatDiv');
-		box.scrollTop = box.scrollHeight;
-	};
-  });
--- server/api/message/message.model.js
+++ server/api/message/message.model.js
@@ -7,7 +7,8 @@ var MessageSchema = new Schema({
   user: {type: String, 
 		 default: "Unknown user"},
   content: String,
-  date: {type: Date, default: Date.now}
+  date: {type: Date, default: Date.now},
+  classroomId: String
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
