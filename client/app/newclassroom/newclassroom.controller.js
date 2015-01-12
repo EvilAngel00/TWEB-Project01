bb045695838b9ca3c62cab6f5d3e9e7192b74aec
Rui Reis <dante2308@gmail.com>
2015-01-12 13:45:01 +0100

    Removed chat-test from repo

--- client/app/newclassroom/newclassroom.controller.js
+++ client/app/newclassroom/newclassroom.controller.js
@@ -5,6 +5,7 @@ angular.module('twebProject01App')
         $scope.isLoggedIn = Auth.isLoggedIn;
 
         $scope.allClassrooms = [];
+        $scope.selectedFile = '';
 
         $http.get('/api/classrooms').success(function (allClassrooms) {
             $scope.allClassrooms = allClassrooms;
@@ -45,5 +46,91 @@ angular.module('twebProject01App')
         $scope.select = function (file) {
             $scope.selected = file;
             console.log($scope.selected.path);
+        }
+
+        $scope.onFileSelect = function (files) {
+            console.log("Hello");
+            var file = '';
+            if (angular.isArray(files)) {
+                file = files[0];
+            }
+
+            $scope.selectedFile = file;
+            console.log($scope.selectedFile);
+
+        };
+
+        $scope.sizeLimit = 10585760; // 10MB in Bytes
+        $scope.uploadProgress = 0;
+        $scope.creds = {};
+
+        $scope.upload = function () {
+            AWS.config.update({
+                accessKeyId: $scope.creds.access_key,
+                secretAccessKey: $scope.creds.secret_key
+            });
+            AWS.config.region = 'us-east-1';
+            var bucket = new AWS.S3({
+                params: {
+                    Bucket: $scope.creds.bucket
+                }
+            });
+            console.log($scope.file);
+
+            if ($scope.file) {
+                // Perform File Size Check First
+                var fileSize = Math.round(parseInt($scope.file.size));
+                if (fileSize > $scope.sizeLimit) {
+                    toastr.error('Sorry, your attachment is too big. <br/> Maximum ' + $scope.fileSizeLabel() + ' file attachment allowed', 'File Too Large');
+                    return false;
+                }
+                // Prepend Unique String To Prevent Overwrites
+                var uniqueFileName = $scope.uniqueString() + '-' + $scope.file.name;
+
+                var params = {
+                    Key: uniqueFileName,
+                    ContentType: $scope.file.type,
+                    Body: $scope.file,
+                    ServerSideEncryption: 'AES256'
+                };
+
+                bucket.putObject(params, function (err, data) {
+                    if (err) {
+                        toastr.error(err.message, err.code);
+                        return false;
+                    } else {
+                        // Upload Successfully Finished
+                        toastr.success('File Uploaded Successfully', 'Done');
+
+                        // Reset The Progress Bar
+                        setTimeout(function () {
+                            $scope.uploadProgress = 0;
+                            $scope.$digest();
+                        }, 4000);
+                    }
+                })
+                    .on('httpUploadProgress', function (progress) {
+                        $scope.uploadProgress = Math.round(progress.loaded / progress.total * 100);
+                        $scope.$digest();
+                    });
+            } else {
+                // No File Selected
+                toastr.error('Please select a file to upload');
+            }
+        }
+
+        $scope.fileSizeLabel = function () {
+            // Convert Bytes To MB
+            return Math.round($scope.sizeLimit / 1024 / 1024) + 'MB';
+        };
+
+        $scope.uniqueString = function () {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for (var i = 0; i < 8; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            return text;
         }
     });
